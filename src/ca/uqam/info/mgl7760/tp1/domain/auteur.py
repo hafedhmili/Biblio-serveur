9a8780e39e9dc4a44cebca6d68f9257533e1f903
hafedhmili <38698840+hafedhmili@users.noreply.github.com>
2024-03-03 21:47:02 -0500

    installed SQLAlchemy, and re-defined all the classes using the ORM declasrative format

--- src/ca/uqam/info/mgl7760/tp1/domain/auteur.py
+++ src/ca/uqam/info/mgl7760/tp1/domain/auteur.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from sqlalchemy import String
 from ca.uqam.info.mgl7760.tp1.domain.basebiblio import BaseBiblio
 from typing import List
 from typing import Optional
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
@@ -18,7 +18,7 @@ class Auteur(BaseBiblio):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     nom: Mapped[str] = mapped_column(String(30))
-    livres: Mapped[List["Livre"]] = relationship(back_populates="Auteur")
+    livres: Mapped[List["Livre"]] = relationship(back_populates="auteurs")
 
 
     @classmethod
